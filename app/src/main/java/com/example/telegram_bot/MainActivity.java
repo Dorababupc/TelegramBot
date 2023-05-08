8813b861ef5b3eba21f988ad4cf886604508edac
Dorababu Arigi <dorababuarigi765@gmail.com>
2023-05-08 15:02:48 +0530

    Final working app beta

--- app/src/main/java/com/example/telegram_bot/MainActivity.java
+++ app/src/main/java/com/example/telegram_bot/MainActivity.java
@@ -3,14 +3,53 @@ package com.example.telegram_bot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText botTokenEditText;
+    private EditText chatIdEditText;
+    private Button submitButton;
+    private TextView resultTextView;
+    private static final String PREFS_NAME = "myPrefs";
+    private static final String KEY_BOT_TOKEN = "bot_token";
+    private static final String KEY_CHAT_ID = "chat_id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botTokenEditText = findViewById(R.id.botTokenInput);
+        chatIdEditText = findViewById(R.id.chatIdInput);
+        submitButton = findViewById(R.id.submitButton);
+        resultTextView = findViewById(R.id.resultMessage);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String botToken = botTokenEditText.getText().toString();
+                String chatId = chatIdEditText.getText().toString();
+
+                if (TextUtils.isEmpty(botToken) || TextUtils.isEmpty(chatId)) {
+                    Toast.makeText(MainActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(KEY_BOT_TOKEN, botToken);
+                editor.putString(KEY_CHAT_ID, chatId);
+                editor.apply();
+
+                // Show a Toast message to confirm that the values have been saved
+                Toast.makeText(MainActivity.this, "Bot automation settings saved", Toast.LENGTH_SHORT).show();
+            }
+        });
         startService(new Intent(this, SMSService.class));
     }
 }
--- app/src/main/java/com/example/telegram_bot/SMSReceiver.java
+++ app/src/main/java/com/example/telegram_bot/SMSReceiver.java
@@ -3,6 +3,7 @@ package com.example.telegram_bot;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
@@ -17,6 +18,11 @@ import java.net.URLConnection;
 
 public class SMSReceiver extends BroadcastReceiver {
     private static final String TAG = "SMSReceiver";
+    private static final String PREFS_NAME = "sms_receiver_prefs";
+    private static final String PREF_LAST_MESSAGE_TIME = "last_message_time";
+
+    private static final String KEY_BOT_TOKEN = "bot_token";
+    private static final String KEY_CHAT_ID = "chat_id";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -25,11 +31,23 @@ public class SMSReceiver extends BroadcastReceiver {
         if (bundle != null) {
             Object[] pdus = (Object[]) bundle.get("pdus");
             if (pdus != null) {
+                long currentTime = System.currentTimeMillis();
+                SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                long lastMessageTime = prefs.getLong(PREF_LAST_MESSAGE_TIME, 0);
+                StringBuilder messageBuilder = new StringBuilder();
+                String senderNumber = "";
                 for (Object pdu : pdus) {
                     SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
-                    String message = smsMessage.getMessageBody();
-                    Log.d(TAG, "Received SMS message: " + message);
-                    new TelegramSenderAsyncTask().execute(context, message);
+                    messageBuilder.append(smsMessage.getMessageBody());
+                    senderNumber = smsMessage.getOriginatingAddress();
+                }
+                String message = messageBuilder.toString();
+                Log.d(TAG, "Received SMS message: " + message);
+                if (currentTime - lastMessageTime >= 100) {
+                    new TelegramSenderAsyncTask().execute(context, message, senderNumber);
+                    prefs.edit().putLong(PREF_LAST_MESSAGE_TIME, currentTime).apply();
+                } else {
+                    Log.d(TAG, "Skipping message forwarding because it's too soon since the last message.");
                 }
             }
         }
@@ -41,14 +59,16 @@ public class SMSReceiver extends BroadcastReceiver {
         protected Void doInBackground(Object... params) {
             Context context = (Context) params[0];
             String msg = (String) params[1];
+            String name = (String) params[2];
             Log.d(TAG, msg);
             String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
 
-            String apiToken = "6054626631:AAHOgTguVmZHtbXpOhkahf965ZS2lBKkuJo";
 
-            //Add chatId (given chatId is fake)
-            String chatId = "-1001760717939";
-            String text = msg;
+            SharedPreferences prefs = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+            String apiToken = prefs.getString(KEY_BOT_TOKEN, "");
+            String chatId = prefs.getString(KEY_CHAT_ID, "");
+            // Concatenate sender number and message with a line separator
+            String text = "From: " + name + "-"+  "\t" + msg;
 
             urlString = String.format(urlString, apiToken, chatId, text);
 
@@ -64,3 +84,4 @@ public class SMSReceiver extends BroadcastReceiver {
         }
     }
 }
+
